b4ae3e18d680b94d6e05af35eb9d9b9a04bc8861
Christopher Guerrero <chrisgrr3@gmail.com>
2020-12-17 22:30:16 -0800

    Update dec_7.js

--- Stacks/dec_7.js
+++ Stacks/dec_7.js
@@ -12,13 +12,37 @@ class slStack {
     }
 
     push(newNode) {
-        newNode.next = this.top;
-        this.top = newNode;
+        if (this.top === null) {
+            this.top = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
     }
 
     pop() {
+        if (this.top === null) return null;
         var first = this.top;
         this.top = this.top.next;
+        first.next = null;
+        this.length--;
         return first;
+    }
+
+    peek() {
+        console.log(this.top.data);
+    }
+
+    isEmpty() {
+        if (this.head === null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    length() {
+        return this.length;
     }
 }
