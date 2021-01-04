7e0c3bc60d73298cdc02af3bc8c541c6a0eb5881
Christopher Guerrero <chrisgrr3@gmail.com>
2021-01-03 22:45:51 -0900

    Create bubbleSort.js

---
+++ Sorts/bubbleSort.js
@@ -0,0 +1,16 @@
+var bubbleSort = array => {
+    let isSorted = false;
+    let temp;
+    while(isSorted == false) {
+        isSorted = true;
+        for(let i = 0; i < array.length-1; i++) {
+            if(array[i] > array[i+1]) {
+                temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+                isSorted = false;
+            }
+        }
+    }
+    return array;
+}
